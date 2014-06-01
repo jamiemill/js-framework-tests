974d76abd96613395966bb01f0164913b51a30e6
Jamie Mill <jamiermill@gmail.com>
2014-06-01 18:48:49 +0100

    Respond to fake stock route

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.use(express.static(__dirname));
 
-// app.get('/', function(req, res){
-//     res.send('hello world');
-// });
+app.get('/stock/5', function(req, res){
+   res.sendfile('index.html');
+});
 
 app.listen(3000);
