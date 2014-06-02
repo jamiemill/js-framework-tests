3fc9739687e15a63b44c50f850301e4a40d32e5e
Jamie Mill <jamiermill@gmail.com>
2014-06-02 21:57:54 +0100

    Add some logging to the server so I can tell it's active more easily

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next){
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+
 app.use(express.static(__dirname));
+
 
 // app.get('/', function(req, res){
 //     res.send('hello world');
 // });
-
-app.listen(3000);
+var port = 3000;
+app.listen(port);
+console.log('server listening on port ' + port);
